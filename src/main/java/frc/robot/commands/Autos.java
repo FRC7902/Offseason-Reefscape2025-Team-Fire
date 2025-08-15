a6d44a85c35432754e81cfaa666ad0ac26d319d5
deathslayer5-dev <deathslayer532@gmail.com>
2025-08-15 16:38:43 -0400

    added paths

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,13 +4,13 @@
 
 package frc.robot.commands;
 
-// import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ExampleSubsystem;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  // public static Command exampleAuto(ExampleSubsystem subsystem) {
-    // return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-  // }
+  
+  public static Command exampleAuto(ExampleSubsystem subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
