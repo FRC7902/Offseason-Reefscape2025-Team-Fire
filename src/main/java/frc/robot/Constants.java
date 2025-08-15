79aab84c174747ac1afd7038e103e50effbf3c29
aadhyasriram <152321380+aadhyasriram@users.noreply.github.com>
2025-08-15 15:51:49 -0400

    Added Left and Right Strafe Commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,7 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     public static final int kOperatorControllerPort = 1;
-    public static final int DEADBAND = 15;
+    public static final double DEADBAND = 0.15;
   }
 
   public static class SwerveConstants {
