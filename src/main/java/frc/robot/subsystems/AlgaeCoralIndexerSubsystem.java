fbd8e24e870ebf5f5d8d6ae29a74e4411545fc63
biggestback <your-email@example.com>
2025-08-23 20:02:28 -0400

    First Commit

--- src/main/java/frc/robot/subsystems/AlgaeCoralIndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeCoralIndexerSubsystem.java
@@ -9,7 +9,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class AlgaeCoralIndexerSubsystem extends SubsystemBase {
   /** Creates a new AlgaeCoralIndexerSubsystem. */
   public AlgaeCoralIndexerSubsystem() {}
-
+  // blah blah blah
+  // coding
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
