584f6d0832712828f82ff40e79f0eb24b7e1335a
deathslayer5-dev <deathslayer532@gmail.com>
2025-09-01 15:59:24 -0400

    Rotates when straight Line :(

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,10 +14,9 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
-
+private final RobotContainer m_robotContainer;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -54,7 +53,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    
+
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
