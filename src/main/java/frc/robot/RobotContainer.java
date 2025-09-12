641df04e612f257c28c688888aff2a50bc5e202e
LimeTheDerg <emilsahel@gmail.com>
2025-09-12 06:08:03 -0400

    Implement Safety #2 From Zack's List
    I am too dead its literally 2AM to try to implement more safeties.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,10 +121,10 @@ public class RobotContainer {
     m_driverController.rightBumper().onTrue(new MoveElevatorArmCommand(ElevatorPosition.ALGAE_HIGH));
     m_driverController.leftBumper().onTrue(new MoveElevatorArmCommand(ElevatorPosition.ALGAE_LOW));
 
-    m_driverController.rightStick().onTrue(new MoveElevatorArmCommand(ElevatorPosition.ZERO));
+    m_driverController.povDown().onTrue(new MoveElevatorArmCommand(ElevatorPosition.ZERO));
     m_driverController.leftStick().onTrue(new MoveElevatorArmCommand(ElevatorPosition.BARGE));
 
-    m_driverController.povDown().onTrue(new MoveElevatorArmCommand(ElevatorPosition.PROCESSOR));
+    m_driverController.rightStick().onTrue(new MoveElevatorArmCommand(ElevatorPosition.PROCESSOR));
     // m_driveSubsystem.setDefaultCommand(new ArcadeDriveCommand(m_driveSubsystem,
     // m_driverController));
   }
