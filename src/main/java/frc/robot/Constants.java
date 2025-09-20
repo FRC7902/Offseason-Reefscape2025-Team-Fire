98979b34daf5e6cca9bd595cd08a26486120b263
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2025-09-20 18:22:37 -0400

    working mary ward pt 1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
     }
 
     public static class SwerveConstants {
-        public static final double MAX_SPEED = Units.feetToMeters(15);
+        public static final double MAX_SPEED = Units.feetToMeters(30);
 
         // Speed scaling factors, should be between 0 and 1
         public static final double MIN_TRANSLATION_SPEED_SCALE = 0.25; // Minimum speed scaling factor for joystick input
