06eb2021e9f72eda8b47a89faba6fdd2af2e6a79
Willie Pai <PaisWillie@gmail.com>
2025-09-20 13:40:44 -0400

    feat: update end effector current limit to 100 and 50

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -56,8 +56,8 @@ public final class Constants {
         public static final int CORAL_BEAM_BREAK_PORT_ID = 0;
         public static final int ALGAE_PROXIMITY_SENSOR_PORT_ID = -1;
 
-        public static final int MOTOR_STATOR_CURRENT_LIMIT = 40;
-        public static final int MOTOR_SUPPLY_CURRENT_LIMIT = 20;
+        public static final int MOTOR_STATOR_CURRENT_LIMIT = 100;
+        public static final int MOTOR_SUPPLY_CURRENT_LIMIT = 50;
 
         public static final double INTAKE_SPEED = 1;
 
