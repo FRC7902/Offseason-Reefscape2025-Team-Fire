79b372a899454251220ccc89b14193cd0486e68f
Willie Pai <PaisWillie@gmail.com>
2025-10-12 20:08:17 -0400

    fix: linting

--- src/main/java/frc/robot/commands/vision/AutoAlignToReef.java
+++ src/main/java/frc/robot/commands/vision/AutoAlignToReef.java
@@ -16,87 +16,90 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AutoAlignToReef extends Command {
-  private PIDController xController, yController, rotController;
-  private boolean isRightScore;
-  private Timer dontSeeTagTimer, stopTimer;
-  private SwerveSubsystem drivebase;
-  private double tagID = -1;
-  /** Creates a new AutoAlignToReef. */
-  public AutoAlignToReef(boolean isRightScore) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    xController = new PIDController(Constants.VisionConstants.X_REEF_ALIGNMENT_P, 0.0, 0);  // Vertical movement
-    yController = new PIDController(Constants.VisionConstants.Y_REEF_ALIGNMENT_P, 0.0, 0);  // Horitontal movement
-    rotController = new PIDController(Constants.VisionConstants.ROT_REEF_ALIGNMENT_P, 0, 0);  // Rotation
-    this.isRightScore = isRightScore;
-    this.drivebase = RobotContainer.m_swerveSubsystem;
-    addRequirements(RobotContainer.m_swerveSubsystem);
-  }
+    private PIDController xController, yController, rotController;
+    private boolean isRightScore;
+    private Timer dontSeeTagTimer, stopTimer;
+    private SwerveSubsystem drivebase;
+    private double tagID = -1;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    this.stopTimer = new Timer();
-    this.stopTimer.start();
-    this.dontSeeTagTimer = new Timer();
-    this.dontSeeTagTimer.start();
-
-    rotController.setSetpoint(Constants.VisionConstants.ROT_SETPOINT_REEF_ALIGNMENT);
-    rotController.setTolerance(Constants.VisionConstants.ROT_TOLERANCE_REEF_ALIGNMENT);
-
-    xController.setSetpoint(Constants.VisionConstants.X_SETPOINT_REEF_ALIGNMENT);
-    xController.setTolerance(Constants.VisionConstants.X_TOLERANCE_REEF_ALIGNMENT);
-
-    yController.setSetpoint(isRightScore ? Constants.VisionConstants.Y_SETPOINT_REEF_ALIGNMENT : -Constants.VisionConstants.Y_SETPOINT_REEF_ALIGNMENT);
-    yController.setTolerance(Constants.VisionConstants.Y_TOLERANCE_REEF_ALIGNMENT);
-
-    tagID = LimelightHelpers.getFiducialID("");
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (LimelightHelpers.getTV("") && LimelightHelpers.getFiducialID("") == tagID) {
-      this.dontSeeTagTimer.reset();
-
-      double[] positions = LimelightHelpers.getBotPose_TargetSpace("");
-
-      SmartDashboard.putNumber("AutoAlign - target x", positions[2]);
-      SmartDashboard.putNumber("AutoAlign - target y", positions[0]);
-      SmartDashboard.putNumber("AutoAlign - target rot", positions[4]);
-
-      SmartDashboard.putNumber("AutoAlign - x", positions[2]);
-
-      double xSpeed = xController.calculate(positions[2]);
-      SmartDashboard.putNumber("AutoAlign - xSpeed", xSpeed);
-      double ySpeed = -yController.calculate(positions[0]);
-      SmartDashboard.putNumber("AutoAlign - ySpeed", ySpeed);
-      double rotValue = -rotController.calculate(positions[4]);
-      SmartDashboard.putNumber("AutoAlign - rotValue", rotValue);
-
-      drivebase.drive(new Translation2d(xSpeed, ySpeed), rotValue, false);
-
-      if (!rotController.atSetpoint() ||
-          !yController.atSetpoint() ||
-          !xController.atSetpoint()) {
-        stopTimer.reset();
-      }
-    } else {
-      drivebase.drive(new Translation2d(), 0, false);
+    /**
+     * Creates a new AutoAlignToReef.
+     */
+    public AutoAlignToReef(boolean isRightScore) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        xController = new PIDController(Constants.VisionConstants.X_REEF_ALIGNMENT_P, 0.0, 0);  // Vertical movement
+        yController = new PIDController(Constants.VisionConstants.Y_REEF_ALIGNMENT_P, 0.0, 0);  // Horitontal movement
+        rotController = new PIDController(Constants.VisionConstants.ROT_REEF_ALIGNMENT_P, 0, 0);  // Rotation
+        this.isRightScore = isRightScore;
+        this.drivebase = RobotContainer.m_swerveSubsystem;
+        addRequirements(RobotContainer.m_swerveSubsystem);
     }
 
-    SmartDashboard.putNumber("poseValidTimer", stopTimer.get());
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        this.stopTimer = new Timer();
+        this.stopTimer.start();
+        this.dontSeeTagTimer = new Timer();
+        this.dontSeeTagTimer.start();
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    drivebase.drive(new Translation2d(), 0, false);
-  }
+        rotController.setSetpoint(Constants.VisionConstants.ROT_SETPOINT_REEF_ALIGNMENT);
+        rotController.setTolerance(Constants.VisionConstants.ROT_TOLERANCE_REEF_ALIGNMENT);
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return this.dontSeeTagTimer.hasElapsed(Constants.VisionConstants.DONT_SEE_TAG_WAIT_TIME) ||
-        stopTimer.hasElapsed(Constants.VisionConstants.POSE_VALIDATION_TIME);
-  }
+        xController.setSetpoint(Constants.VisionConstants.X_SETPOINT_REEF_ALIGNMENT);
+        xController.setTolerance(Constants.VisionConstants.X_TOLERANCE_REEF_ALIGNMENT);
+
+        yController.setSetpoint(isRightScore ? Constants.VisionConstants.Y_SETPOINT_REEF_ALIGNMENT : -Constants.VisionConstants.Y_SETPOINT_REEF_ALIGNMENT);
+        yController.setTolerance(Constants.VisionConstants.Y_TOLERANCE_REEF_ALIGNMENT);
+
+        tagID = LimelightHelpers.getFiducialID("");
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        if (LimelightHelpers.getTV("") && LimelightHelpers.getFiducialID("") == tagID) {
+            this.dontSeeTagTimer.reset();
+
+            double[] positions = LimelightHelpers.getBotPose_TargetSpace("");
+
+            SmartDashboard.putNumber("AutoAlign - target x", positions[2]);
+            SmartDashboard.putNumber("AutoAlign - target y", positions[0]);
+            SmartDashboard.putNumber("AutoAlign - target rot", positions[4]);
+
+            SmartDashboard.putNumber("AutoAlign - x", positions[2]);
+
+            double xSpeed = xController.calculate(positions[2]);
+            SmartDashboard.putNumber("AutoAlign - xSpeed", xSpeed);
+            double ySpeed = -yController.calculate(positions[0]);
+            SmartDashboard.putNumber("AutoAlign - ySpeed", ySpeed);
+            double rotValue = -rotController.calculate(positions[4]);
+            SmartDashboard.putNumber("AutoAlign - rotValue", rotValue);
+
+            drivebase.drive(new Translation2d(xSpeed, ySpeed), rotValue, false);
+
+            if (!rotController.atSetpoint() ||
+                    !yController.atSetpoint() ||
+                    !xController.atSetpoint()) {
+                stopTimer.reset();
+            }
+        } else {
+            drivebase.drive(new Translation2d(), 0, false);
+        }
+
+        SmartDashboard.putNumber("poseValidTimer", stopTimer.get());
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        drivebase.drive(new Translation2d(), 0, false);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return this.dontSeeTagTimer.hasElapsed(Constants.VisionConstants.DONT_SEE_TAG_WAIT_TIME) ||
+                stopTimer.hasElapsed(Constants.VisionConstants.POSE_VALIDATION_TIME);
+    }
 }
